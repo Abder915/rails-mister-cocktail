8a76b7978a29cba6b12047696dfbcadcee8815bf
Abder Nadifi <nabder@naditel.com>
2020-03-13 16:07:50 +0100

    image Cloudinary

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -19,6 +19,7 @@ class CocktailsController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
@@ -26,7 +27,6 @@ class CocktailsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
-
 end
